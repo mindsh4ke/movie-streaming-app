bda95033c213f27fb0a7ef58eec236c05e7c2acc
mindsh4ke <105060443+mindsh4ke@users.noreply.github.com>
2023-09-26 21:08:09 +0200

    Delete src/configuration.js

--- src/configuration.js
+++
@@ -1,7 +0,0 @@
-export const getMethodOptions = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTFhMzJmOTE3ZWZlY2JjMDJmZTNlMWM2MTMzOWEyNCIsInN1YiI6IjYzZGQ1YmVkZGRkNTJkMDA3YzgxNWVhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ambe02BlfF8AR3QLKT7AfCWAYKgIa6oNGQzU6M9PHX4'
-    }
-};
